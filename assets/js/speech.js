0f738c3cef51ed18c908103faeb6b4bae502bc46
Tim Nelson <traveltimn@gmail.com>
2023-03-30 22:05:14 +0100

    Add speech synth for 15 new languages

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -6,30 +6,43 @@ let langCodes = [];
 let theWord, theWordLang, voices;
 let speechAvailable = false;
 const btnAudio = by.id("btn-audio");
-const languages = ["en", "ar", "zh", "nl", "fr", "de", "el", "hi", "is", "ie", "it", "ja", "ko", "nb", "pl", "pt", "ro", "ru", "es", "sv", "tr", "vi"];
+const languages = ["ar", "zh", "dk", "nl", "en", "ph", "fi", "fr", "de", "gr", "il", "hi", "hu", "is", "id", "ie", "it", "ja", "ko", "lv", "lt", "my", "no", "pl", "pt", "ro", "ru", "rs", "sk", "es", "sv", "th", "tr", "ua", "vi"];
 
 // languages in order of spoken preference
 const ar = ["ar-SA", "ar_SA", "ar"];
 const zh = ["zh-CN", "zh_CN_#Hans", "zh", "zh-HK", "zh_HK_#Hans", "zh_HK_#Hant", "yue_HK_#Hant", "zh-TW", "zh_TW_#Hant"];
+const da = ["da-DK", "da_DK", "dk"];
 const nl = ["nl-NL", "nl_NL", "nl", "nl-BE", "nl_BE"];
 const en = ["en-GB", "en_GB", "en-IE", "en_IE", "en-scotland", "en_scotland", "en", "en-US", "en_US", "en-AU", "en_AU", "en-ZA", "en_ZA", "en-IN", "en_IN", "en-NG", "en_NG"];
+const ph = ["fil-PH", "fil_PH", "ph"];
+const fi = ["fi-FI", "fi_FI", "fi"];
 const fr = ["fr-FR", "fr_FR", "fr", "fr-CA", "fr_CA"];
 const de = ["de-DE", "de_DE", "de", "de-AT", "de_AT", "de-LI", "de_LI", "de-CH", "de_CH"];
-const el = ["el-GR", "el_GR", "el"];
+const gr = ["el-GR", "el_GR", "el"];
+const il = ["he-IL", "he_IL", "il"];
 const hi = ["hi-IN", "hi_IN", "hi"];
-const is = ["is-IS", "is_IS", "is"]; 
-const ie = [];
+const hu = ["hu-HU", "hu_HU", "hu"];
+const is = ["is-IS", "is_IS", "is"];
+const id = ["id-ID", "id_ID", "id"];
+const ie = ["ga-IE", "ga_IE", "ie"];
 const it = ["it-IT", "it_IT", "it"];
 const ja = ["ja-JP", "ja_JP", "ja"];
 const ko = ["ko-KR", "ko_KR", "ko"];
-const nb = ["nb-NO", "nb_NO", "nb", "no-NO", "no_NO", "nn-NO", "nn_NO", "nn"];
+const lv = ["lv-LV", "lv_LV", "lv"];
+const lt = ["lt-LT", "lt_LT", "lt"];
+const my = ["ms-MY", "ms_MY", "my"];
+const no = ["nb-NO", "nb_NO", "nb", "no-NO", "no_NO", "nn-NO", "nn_NO", "nn"];
 const pl = ["pl-PL", "pl_PL", "pl"];
 const pt = ["pt-BR", "pt_BR", "pt", "pt-PT", "pt_PT"];
 const ro = ["ro-RO", "ro_RO", "ro"];
 const ru = ["ru-RU", "ru_RU", "ru"];
+const rs = ["rs-RS", "rs_RS", "rs"];
+const sk = ["sk-SK", "sk_SK", "sk"];
 const es = ["es-MX", "es_MX", "es-ES", "es_ES", "es", "es-US", "es_US", "es-AR", "es_AR"];
 const sv = ["sv-SE", "sv_SE", "sv"];
+const th = ["th-TH", "th_TH", "th"];
 const tr = ["tr-TR", "tr_TR", "tr"];
+const ua = ["uk-UR", "uk_UA", "ua"];
 const vi = ["vi-VN", "vi_VN", "vi"];
 
 
